0af98ed96ad631745d232c0bb475daee57953777
Sohan <sohan39@gmail.com>
2021-12-17 10:50:45 -0800

    adds an env variable to see if it works on both Cloud Run and GAE

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/smsohan/gcp-rampup
 
-go 1.17
+go 1.16
--- main.go
+++ main.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
 type Rampup struct {
-	Time    time.Time `json:"time"`
+	Time   time.Time `json:"time"`
 	Task   string    `json:"task"`
 	Status string    `json:"status"`
 }
 
 func ApiHandler(w http.ResponseWriter, r *http.Request) {
+
+	runtime, isProvided := os.LookupEnv("RUNTIME")
+	if !isProvided {
+		runtime = "Localhost"
+	}
+
 	goRampup := Rampup{
-		Time:    time.Now(),
+		Time:   time.Now(),
 		Task:   "Re-Learn Go",
-		Status: "Deployed on CloudRun",
+		Status: fmt.Sprintf("Deployed on %s", runtime),
 	}
 	json.NewEncoder(w).Encode(goRampup)
 }
