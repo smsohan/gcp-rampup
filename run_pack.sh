fa1d707d435b8bfa0a32f6dfaafeaf06c5ce245b
Sohan <sohan39@gmail.com>
2021-12-22 10:27:24 -0800

    adds BOM and publishes the buildpack image

--- run_pack.sh
+++ run_pack.sh
@@ -3,4 +3,12 @@ set -x
 
 pack config default-builder cnbs/sample-builder:bionic
 pack build test-go-app --path . --buildpack ./go-buildpack
+
+
+# pack buildpack package smsohan/go-cnb --config ./package.toml
+# pack buildpack package smsohan/go-cnb --config ./package.toml --publish
+# pack buildpack package go-buildpack.cnb --config ./package.toml --format file
+
+# pack buildpack register smsohan/go-cnb
+
 docker run -p 8080:8080 test-go-app
